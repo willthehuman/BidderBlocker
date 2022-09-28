6903bbb6ca3edfe99521bc314c46aa12f25c05b1
willthehuman <williamcperrier@gmail.com>
2022-09-28 16:58:25 -0400

    Unused imports

--- azb.py
+++ azb.py
@@ -1,9 +1,6 @@
 import sys
-import this
-import selenium # Using Chrome to access web
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
